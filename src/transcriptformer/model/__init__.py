7c50898a7d64d3df90e4b76d0e4b3f5dbb8d2682
Steven ten Holder <doctorduality@gmail.com>
2025-05-19 15:36:50 -0700

    Expose Transcriptformer and LoRA utilities in model init

--- src/transcriptformer/model/__init__.py
+++ src/transcriptformer/model/__init__.py
@@ -3,8 +3,10 @@
 from __future__ import annotations
 
 from .lora import LoRAConfig, LoRALinear, apply_lora, iter_lora_layers, lora_state_dict
+from .model import Transcriptformer
 
 __all__ = [
+    "Transcriptformer",
     "LoRALinear",
     "LoRAConfig",
     "apply_lora",
